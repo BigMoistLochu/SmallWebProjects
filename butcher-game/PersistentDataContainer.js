d395f042bffd364a9b463953dd6a59ebaf0acf42
BigMoistLochu <krok.konrad1@gmail.com>
2025-02-21 18:53:50 +0100

    added persistance data players

--- butcher-game/PersistentDataContainer.js
+++ butcher-game/PersistentDataContainer.js
@@ -1,1 +1,9 @@
-//todo: glowna klasa w ktorej jest stan Player i innych trwalych zmian
+import { Player } from "./Player.js";
+
+const player1 = new Player(1,"Konrad");
+const player2 = new Player(2,"Pawel");
+const player3 = new Player(3,"James");
+
+const players = [player1,player2,player3];
+
+
